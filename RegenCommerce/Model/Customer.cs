65ade5658af186509acf42d43ba59d86c5caef85
dimitris <iracleous@gmail.com>
2021-11-08 19:17:40 +0200

    Add project files.

---
+++ RegenCommerce/Model/Customer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegenCommerce.Model
+{
+    public class Customer
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public decimal Balance { get; set; }
+        public int TransactionsCount { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
---
+++ RegenCommerce/Program.cs
@@ -0,0 +1,39 @@
+﻿using RegenCommerce.Model;
+using System;
+
+namespace RegenCommerce
+{
+    public static class MyProgram
+    {
+        public static void Main(string[] args)
+        {
+            string greeting = "Hello ";
+           
+            var customer = new Customer 
+            {
+                FirstName = "Ioannis",
+                LastName = "Eleftheriou",
+                Balance = 10,
+                BirthDate = new DateTime(1970, 12, 5)
+            };
+
+
+            var cust2 = customer;
+
+           
+
+
+            var cust3 = new Customer
+            {
+                FirstName = customer.FirstName,
+                LastName =  customer.LastName,
+                Balance = 0,
+                BirthDate = new DateTime(1980, 12, 5)
+            };
+                customer.FirstName = "Eirini";
+
+            Console.WriteLine(greeting + cust3.FirstName);
+
+        }
+    }
+}
